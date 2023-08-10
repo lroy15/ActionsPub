369e43d8fb8538f4881da4747d19eebab2e9240f
lroy15 <laurentjulien.roy@gmail.com>
2023-08-10 16:15:04 -0400

    First commit

---
+++ web/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>wow isn't NGINX fun? 😔 lol fuaadge</p>
+        <p>
+          <a href="/api/getData">🤫 super secret database here 🤫</a>
+        </p>
+      </section>
+    </Layout>
+  );
+}
